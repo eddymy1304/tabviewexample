abf2e42585a043a89eb4c1ff1dd76ca7719d4198
Eddy Mendoza <eddymy1304@gmail.com>
2024-12-20 22:54:56 -0500

    Initial Commit

---
+++ tabviewexample/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  tabviewexample
+//
+//  Created by Eddy Mendoza on 20/12/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ tabviewexample/tabviewexampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  tabviewexampleApp.swift
+//  tabviewexample
+//
+//  Created by Eddy Mendoza on 20/12/24.
+//
+
+import SwiftUI
+
+@main
+struct tabviewexampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
